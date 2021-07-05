6ae2d957b0435000c37f8f6f4816c688e2fcb0d1
gaoyuan <707216098@qq.com>
2021-07-05 12:17:22 +0800

    add

---
+++ DesignPatterns/DPEntrance.swift
@@ -0,0 +1,19 @@
+//
+//  DPEntrance.swift
+//  iphoneCom
+//
+//  Created by gaoyuan on 2021/1/27.
+//
+
+import UIKit
+
+public class DPEntrance: NSObject {
+    
+    public class func commandPattern() {
+        print("1111")
+    }
+    
+    public class func statusPattern() {
+        print("22222")
+    }
+}
