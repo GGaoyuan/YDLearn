11adc432dcee50ee9e654d8b04b11b9762507747
gaoyuan <707216098@qq.com>
2021-10-28 14:53:39 +0800

    add

---
+++ Leetcode/LTEntrance.swift
@@ -0,0 +1,16 @@
+//
+//  LTEntrance.swift
+//  YDLearn
+//
+//  Created by gaoyuan on 2021/10/25.
+//
+
+import Foundation
+
+public class LTEntrance: NSObject {
+    
+    public class func commandPattern() {
+        
+    }
+    
+}
